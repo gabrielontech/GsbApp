d9179d94a1a00705bd7ee2d6b6c54e6c135ef38d
keller19 <gaabrielkeller19@gmail.com>
2021-03-08 19:27:27 +0100

    supressions de la classe AcessHTTP.java, remplacer par la methode VOLLEY

--- app/src/main/java/com/example/gsbapps/MainActivity.java
+++ app/src/main/java/com/example/gsbapps/MainActivity.java
@@ -8,53 +8,76 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
-    EditText userName;
-    EditText userPassword;
+    EditText userName, userPassword;
     Button userConnexion;
+    private String user_name, user_pass;
+    private String URL = "http://192.168.64.2/LoginRegister/validate.php";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+          user_name = user_pass = " ";
           userName = findViewById(R.id.userName);
           userPassword = findViewById(R.id.userPassword);
           userConnexion = findViewById(R.id.connexion);
 
-        userConnexion.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+          userConnexion.setOnClickListener(new View.OnClickListener() {
+              @Override
+              public void onClick(View v) {
+                  login();
+              }
+          });
+    }
 
-                AcessHTTP accesDonnees = new AcessHTTP(){
-                    @Override
-                    protected void onPostExecute(Long result) {
-                        try {
-                            JSONArray tJson = new JSONArray(this.ret);
-                            if (tJson.length()!=0) {
-                                int i = 0;
-                                String v1 =
-                                        tJson.getJSONObject(i).getString("v1");
-                            }
-                        }catch (JSONException e){
-                            Log.d("log","pb decodage JSON "+e.toString());
-                        }
+    public void login(){
+        user_name = userName.getText().toString().trim();
+        user_pass = userPassword.getText().toString().trim();
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    if (response.contains("1")) {
+                        Intent MenuActivityIntent = new Intent(MainActivity.this, Menu.class);
+                        MenuActivityIntent.putExtra("name", userName.getText().toString());
+                        startActivity(MenuActivityIntent);
+                    } else{
+                        Toast.makeText(getApplicationContext(), "Mot de passe ou identifiant invalide", Toast.LENGTH_LONG).show();
                     }
-                };
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                }
+            }){
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    Map<String, String>  params = new HashMap<>();
+                    params.put("username", user_name);
+                    params.put("password", user_pass);
+                    return params;
+                }
+            };
 
-                accesDonnees.addParam("name", userName.getText().toString());
-                accesDonnees.addParam("motDePasse", userPassword.getText().toString());
-
-                accesDonnees.execute("http://192.168.0.50/gsb/verification.php");
-
-                Intent MenuActivityIntent = new Intent(MainActivity.this, Menu.class);
-                MenuActivityIntent.putExtra("name", userName.getText().toString());
-                startActivity(MenuActivityIntent);
-            }
-        });
+            Volley.newRequestQueue(this).add(stringRequest);
 
     }
+
 }
