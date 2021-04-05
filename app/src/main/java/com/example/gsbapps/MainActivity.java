30b459825a7545a44aa5488ba05465ca70f808bb
keller19 <gaabrielkeller19@gmail.com>
2021-04-05 22:51:18 +0200

    avant dernier controle

--- app/src/main/java/com/example/gsbapps/MainActivity.java
+++ app/src/main/java/com/example/gsbapps/MainActivity.java
@@ -41,7 +41,6 @@ public class MainActivity extends AppCompatActivity {
               @Override
               public void onClick(View v) {
                   login();
-
               }
           });
     }
